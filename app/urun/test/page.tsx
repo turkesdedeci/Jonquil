4f4c87de04629e2ab806598c9d7d12ac654de0dc
Süleyman Türkeş Dedeci <turkesdedeci@icloud.com>
2026-02-09 02:22:43 +0300

    Add /urun test page

---
+++ app/urun/test/page.tsx
@@ -0,0 +1,8 @@
+export default function UrunTestPage() {
+  return (
+    <div style={{ padding: 24, fontFamily: 'system-ui, -apple-system, Segoe UI, sans-serif' }}>
+      <h1>Urun Test OK</h1>
+      <p>If you can see this, /urun/* routes are wired correctly.</p>
+    </div>
+  );
+}
